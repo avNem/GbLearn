6d92e86c53267786569ae000cde90e94f416a38d
avNem <lewadebeats@gmail.com>
2022-03-11 17:20:46 +0300

    update br5

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -2,5 +2,6 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         Employee e1 = new Employee("VAS", "Manager", "asd@rr.ru", "11-11",
                 100, 23);
+
     }
 }
